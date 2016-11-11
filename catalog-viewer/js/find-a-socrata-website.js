b2f12d2fbed561967bf3e213bd5fe91985eadf50
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2016-11-11 10:25:53 -0500

    Add column search to Find a website

--- catalog-viewer/js/find-a-socrata-website.js
+++ catalog-viewer/js/find-a-socrata-website.js
@@ -1,3 +1,33 @@
+"use strict";
+
+var columnSearch = {
+    makeColumnsSearchable: function () {
+        // Setup - add a text input to each footer cell
+        $('#catalog_table tfoot th').each(function () {
+            var title = $('#catalog_table thead th').eq($(this).index()).text();
+            $(this).html('<input type="text" class="colSearch" placeholder="Search ' + title + '" />');
+        });
+
+        // Apply the search
+        var dataTable = $('#catalog_table').DataTable();
+        dataTable.columns().every(function () {
+            var that = this;
+            $('.colSearch', this.footer()).on('keyup change', function () {
+                if (that.search() !== this.value) {
+                    that.search(this.value).draw();
+                }
+            });
+        });
+
+        //move footers to the top
+        var row = $('#catalog_table tfoot tr');
+        row.find('th').each(function () {
+            $(this).css('padding', 8);
+        });
+        $('#catalog_table thead').append(row);
+    }
+};
+
 var socrataDomainFinder = {
     showDataTable: function () {
         $('#catalog_table').dataTable( {
@@ -15,13 +45,9 @@ var socrataDomainFinder = {
             { "data" : "count" },
         ]       
     } );
+    columnSearch.makeColumnsSearchable();
 
     }
 }
 
 socrataDomainFinder.showDataTable();
-
-
-$(document).ready(function() {
-} );
-
