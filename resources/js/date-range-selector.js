9ad2c4a4a744fb291c0b6819cb699399c3e5c386
Jeff Chamblee <jeff.chamblee@aquilent.com>
2016-11-20 21:33:26 -0500

    If date is before Dec 7 2013, get Thursday

--- resources/js/date-range-selector.js
+++ resources/js/date-range-selector.js
@@ -11,6 +11,8 @@ var dateFormatter = {
     }
 };
 
+
+//NADAC data is usually published on Wednesday each week
 var dateAdjuster = {
     // change date to Wednesday of the specified week
     getWednesday: function (date) {
@@ -18,14 +20,29 @@ var dateAdjuster = {
         var diff = 3 - date.getDay();
         newDate.setDate(date.getDate() + diff);
         return newDate;
+    },
+    // change date to Thursday of the specified week
+    getThursday: function (date) {
+        var newDate = date;
+        var diff = 4 - date.getDay();
+        newDate.setDate(date.getDate() + diff);
+        return newDate;
+    },
+    getDatePublished: function (dateSelected) {
+        var date1 = new Date("2013-11-24");
+        var date2 = new Date("2013-12-08");
+        if (dateSelected >= date1 && dateSelected < date2) {
+            return this.getThursday(dateSelected);
+        } else {
+            return this.getWednesday(dateSelected);
+        }
     }
 };
 
 var dateRangeSelector = {
     from: $("#datepicker_start").datepicker({
         defaultDate: "-1M",
-        //minDate: "11/28/2013",
-        minDate: "12/08/2013",
+        minDate: "11/24/2013",
         maxDate: "-1d",
         changeMonth: true,
         changeYear: true
@@ -41,13 +58,13 @@ var dateRangeSelector = {
         $("#datepicker_end").val("11/16/2016");
         var parent = this;
         parent.from.on("change", function () {
-            // change date to Wednesday of the specified week
-            $("#datepicker_start").val(dateFormatter.formatMM(dateAdjuster.getWednesday(parent.getDate(this))));
+            // change date to Wednesday of the specified week, or Thursday if appropriate
+            $("#datepicker_start").val(dateFormatter.formatMM(dateAdjuster.getDatePublished(parent.getDate(this))));
             parent.to.datepicker("option", "minDate", parent.getDate(this));
         });
         parent.to.on("change", function () {
-            // change date to Wednesday of the specified week
-            $("#datepicker_end").val(dateFormatter.formatMM(dateAdjuster.getWednesday(parent.getDate(this))));
+            // change date to Wednesday of the specified week, or Thursday if appropriate
+            $("#datepicker_end").val(dateFormatter.formatMM(dateAdjuster.getDatePublished(parent.getDate(this))));
             parent.from.datepicker("option", "maxDate", parent.getDate(this));
         });
     },
