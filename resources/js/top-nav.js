22512bc20b9fc255b5a78c618cd5fb3509d11b12
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2017-09-14 17:00:11 -0400

    Fix top nav

--- resources/js/top-nav.js
+++ resources/js/top-nav.js
@@ -1,19 +1,27 @@
 "use strict";
 
-//buttons
-$("#open-data-1").click(function(){
-    window.location.href = "/catalog-viewer/SocrataDomains.html";
-}); 
-$("#open-data-2").click(function(){
-    window.location.href = "/catalog-viewer/DataCatalogSearch.html";
-}); 
-$("#web-app-security").click(function(){
-    window.location.href = "/owasp-quiz/OWASPQuiz1.html";
-}); 
-$("#data-viz").click(function(){
-    window.location.href = "/medicaid/charts/index.html";
-}); 
-$("#drug-price").click(function(){
-    window.location.href = "/drug-prices/DrugPriceIncrease.html";
-}); 
+var topNavButtons = {
+        createListeners: function() {
+                //buttons
+                $("#open-data-1").click(function(){
+                    window.location.href = "/catalog-viewer/SocrataDomains.html";
+                }); 
+                $("#open-data-2").click(function(){
+                    window.location.href = "/catalog-viewer/DataCatalogSearch.html";
+                }); 
+                $("#web-app-security").click(function(){
+                    window.location.href = "/owasp-quiz/OWASPQuiz1.html";
+                }); 
+                $("#data-viz").click(function(){
+                    window.location.href = "/medicaid/charts/index.html";
+                }); 
+                $("#drug-price").click(function(){
+                    window.location.href = "/drug-prices/DrugPriceIncrease.html";
+                });          
+        }
+};
+
+$(document).ready(function() {
+    topNavButtons.createListeners();
+});
 
