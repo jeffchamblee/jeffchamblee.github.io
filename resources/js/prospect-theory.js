654e3fbe48feecf3e3e127231dbcdce0ced94f83
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2017-09-24 20:41:48 -0400

    Answer 2

--- resources/js/prospect-theory.js
+++ resources/js/prospect-theory.js
@@ -8,13 +8,15 @@ var prospect_theory = {
 		
 		// show answers
 	    $(".answer").css("display:block;");
+	    $(".answer").css("color:blue;");
 		// question 1 
 		if ($('#decision_1 input:radio').val() === '1') {
-			$("#answer1").text("You chose to maximize utility over certainty");
+			$("#answer1").text("You chose maximizing utility over certainty");
 		} else if ($('#decision_1 input:radio').val() === '2') {
 			$("#answer1").text("You chose certainty over maximizing utility");
 		}
 		// question 2
+	    $("#answer2").text("Answer 2 ");
 		if ($('#decision_2 input:radio').val() === '1') {
 			$("#answer2").text("You chose maximizing utility over certainty");
 		} else if ($('#decision_2 input:radio').val() === '2') {
