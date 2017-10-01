b445055df393be088f5f2e561e87a3e362bc936d
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2017-10-01 19:56:57 -0400

    clear answers

--- resources/js/prospect-theory.js
+++ resources/js/prospect-theory.js
@@ -3,13 +3,30 @@ var prospect_theory = {
 	hideAnswers: function() {
 	    $(".answer").css("display:none;");
 	},
+	clearAnswers: function() {
+	    //Clear previous answers
+		$("#answer1").text("");
+		$("#answer2").text("");
+		$("#answer3").text("");
+		$("#answer4").text("");
+		$("#answer5").text("");
+		$("#answer6").text("");
+		$("#answer7").text("");
+		$("#answer8").text(""); 	    
+		$("#answer9").text(""); 	    
+		$("#answer10").text(""); 	    
+		$("#answer11").text("");
+		$("#answer12").text("");
+		$("#answer13").text("");
+	},
 	displayAnswers: function() {
 		//document.getElementById('answer').style.display='block'; 
 		
 		// show answers
 	    $(".answer").css("display:block;");
 	    $(".answer").css("color", "blue");
-	    $("#answers_above").text("See above for answers");
+	    $("#answers_above").text("See above for answers.");
+	    this.clearAnswers();
 
 		// question 1 
 		$("#answer1").text("People underweight outcomes that are merely probable in comparison with outcomes that are obtained with certainty. Most people choose the certain (although smaller) gain. ");
