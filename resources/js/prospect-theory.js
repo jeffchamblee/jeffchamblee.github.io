39fd62aaaba325bf86ecf2203d829e27148a355a
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2017-10-21 08:54:25 -0400

    Added expected values

--- resources/js/prospect-theory.js
+++ resources/js/prospect-theory.js
@@ -12,6 +12,7 @@ var prospect_theory = {
                 '<li id="decision_' + index + '"><label class="description">' + record.question + '</label> <span>' +
                 '<input class="element radio" id="decision_' + index + '_A" name="decision_' + index + '" type="radio" value="1"> <label class="choice" for="decision_' + index + '_A">' + record.choices[0].text + '</label> ' +
                 '<input class="element radio" id="decision_' + index + '_B" name="decision_' + index + '" type="radio" value="2"> <label class="choice" for="decision_' + index + '_B">' + record.choices[1].text + '</label></span></li>' +
+                '<p class="answer" id="expected-value' + index + '">' + '</p>' +
                 '<p class="answer" id="answer' + index + '">' + record.answer + '</p>' +
                 '<p class="answer" id="response' + index + '"></p>'
                 );
@@ -27,8 +28,17 @@ var prospect_theory = {
         $("#answers_above").text("See above for answers.");
         var index = 0;
         var selectedRadio;
+        var expectedValue;
         for (; index < prospect_theory.question_list.length; index++) {
             selectedRadio = $("input[name=decision_" + index + "]:checked", '#decision_problems').val();
+            expectedValue = prospect_theory.question_list[index].choices[0].expectedValue;
+            if (typeof expectedValue !== 'undefined') {
+                $("#expected-value" + index).text("Expected values - A: " + expectedValue);
+            }
+            expectedValue = prospect_theory.question_list[index].choices[1].expectedValue;
+            if (typeof expectedValue !== 'undefined') {
+                $("#expected-value" + index).append(" B: " + expectedValue);
+            }
             if (selectedRadio === '1') {
                 $("#response" + index).text(prospect_theory.question_list[index].choices[0].response);
             } else if (selectedRadio === '2') {
