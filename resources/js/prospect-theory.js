97fe06806f8eb6430d7fd3a7cbc0c9eab352c20d
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2017-10-22 09:01:50 -0400

    fix

--- resources/js/prospect-theory.js
+++ resources/js/prospect-theory.js
@@ -18,7 +18,6 @@ var prospect_theory = {
             });
             prospect_theory.hideAnswers();
         });
-
     },
     displayAnswers: function() {
         $(".answer").css("display", "block");
@@ -26,19 +25,8 @@ var prospect_theory = {
         $("#answers_above").text("See above for answers.");
         var index = 0;
         var selectedRadio;
-        var expectedValue;
         for (; index < prospect_theory.question_list.length; index++) {
             selectedRadio = $("input[name=decision_" + index + "]:checked", '#decision_problems').val();
-            /*
-            expectedValue = prospect_theory.question_list[index].choices[0].expectedValue;
-            if (typeof expectedValue !== 'undefined') {
-                $("#expected-value" + index).text("Expected values - A: " + expectedValue);
-            }
-            expectedValue = prospect_theory.question_list[index].choices[1].expectedValue;
-            if (typeof expectedValue !== 'undefined') {
-                $("#expected-value" + index).append(" B: " + expectedValue);
-            }
-            */
             if (selectedRadio === '0') {
                 $("#response" + index).text(prospect_theory.question_list[index].choices[0].response);
             } else if (selectedRadio === '1') {
@@ -51,12 +39,10 @@ var prospect_theory = {
         var result = "";
         var expectedValue;
         for (; choiceIndex < choiceList.length; choiceIndex++) {
-            result += '<input class="element radio" id="decision_' + index + '_A" name="decision_' + index + '" type="radio" value=" ' + index+ ' "> <label class="choice" for="decision_' + index + '_A">' + choiceList[choiceIndex].text + '</label> ';
+            result += '<input class="element radio" id="decision_' + index + '_A" name="decision_' + index + '" type="radio" value="' + choiceIndex + '"> <label class="choice" for="decision_' + index + '_A">' + choiceList[choiceIndex].text + '</label> ';
             expectedValue = choiceList[choiceIndex].expectedValue;
             if (typeof expectedValue !== 'undefined') {
-                result += '<span class="answer> Expected value: ' + expectedValue + '</span>' ;
-
-               // $("#expected-value" + index + "-"choiceIndex).text(" Expected value: " + expectedValue);
+                //result += '<span class="answer> Expected value: ' + expectedValue + '</span>' ;
             }
         }
         return result;
