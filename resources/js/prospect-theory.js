2acfc9e8d41ff0f17f246ed181b188845ebdadf9
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2017-10-22 20:18:04 -0400

    fix css

--- resources/js/prospect-theory.js
+++ resources/js/prospect-theory.js
@@ -27,7 +27,7 @@ var prospect_theory = {
         var selectedRadio;
         for (; index < prospect_theory.question_list.length; index++) {
             selectedRadio = $("input[name=decision_" + index + "]:checked", '#decision_problems').val();
-            // TODO: allow more than two
+            // TODO: allow more than two choices
             if (selectedRadio === '0') {
                 $("#response" + index).text(prospect_theory.question_list[index].choices[0].response);
             } else if (selectedRadio === '1') {
@@ -40,7 +40,7 @@ var prospect_theory = {
         var result = "";
         var expectedValue;
         for (; choiceIndex < choiceList.length; choiceIndex++) {
-            result += '<label class="choice"> <input class="element radio" name="decision_' + index + '" type="radio" value="' + choiceIndex + '"/>' +  choiceList[choiceIndex].text + '</label> ';
+            result += '<label class="choice_prospect"> <input class="element radio_prospect" name="decision_' + index + '" type="radio" value="' + choiceIndex + '"/>' + choiceList[choiceIndex].text + '</label> ';
             expectedValue = choiceList[choiceIndex].expectedValue;
             if (typeof expectedValue !== 'undefined') {
                 result += '<span class="answer"> Expected value: ' + expectedValue + '</span>';
