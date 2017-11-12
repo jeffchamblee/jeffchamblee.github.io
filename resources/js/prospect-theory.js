e69d84614f9226d386fa60dffe86a1b195549089
Jeff Chamblee <Chamblee@JeffChableesMBP.home>
2017-11-11 21:06:02 -0500

    fix all qs

--- resources/js/prospect-theory.js
+++ resources/js/prospect-theory.js
@@ -60,35 +60,41 @@ var google_forms = {
 		//var email = $('#Email').val();
 		var email = "default@email.com";
 		var response1 = $("input[name='decision_0']:checked").val();
-		response1 = prospect_theory.question_list[0].choices[response1].text;
-		//var response1 = "$2400 for sure";
-		//var response1 = $('input[name="decision_0"]:checked').parent().text();
-		//var response1 = $("input[name='decision_0']:checked").val();
+		var responseText1 = prospect_theory.question_list[0].choices[response1].text;
         console.log("response1=" + response1);
 
 		var response2 = $("input[name='decision_1']:checked").val();
+		var responseText2 = prospect_theory.question_list[1].choices[response2].text;
 		var response3 = $("input[name='decision_2']:checked").val();
+		var responseText3 = prospect_theory.question_list[2].choices[response3].text;
 		var response4 = $("input[name='decision_3']:checked").val();
+		var responseText4 = prospect_theory.question_list[3].choices[response4].text;
 		var response5 = $("input[name='decision_4']:checked").val();
+		var responseText5 = prospect_theory.question_list[4].choices[response5].text;
 		var response6 = $("input[name='decision_5']:checked").val();
+		var responseText6 = prospect_theory.question_list[5].choices[response6].text;
 		var response7 = $("input[name='decision_6']:checked").val();
+		var responseText7 = prospect_theory.question_list[6].choices[response7].text;
 		var response8 = $("input[name='decision_7']:checked").val();
+		var responseText8 = prospect_theory.question_list[7].choices[response8].text;
 		var response9 = $("input[name='decision_8']:checked").val();
+		var responseText9 = prospect_theory.question_list[8].choices[response9].text;
 		var response10 = $("input[name='decision_9']:checked").val();
+		var responseText10 = prospect_theory.question_list[9].choices[response10].text;
     
 		$.ajax({
 			url: "https://docs.google.com/forms/d/e/1FAIpQLSdyuFZN5AZYQfbElV_yXXixRUl2tzUfOFtIrxEx55HZ-Jiv2g/formResponse",
 			data: {
-				"entry.755042765": response1,
-				"entry.1471559929": response2,
-				"entry.1163044351": response3,
-				"entry.1384086246": response4,
-				"entry.879818529": response5,
-				"entry.2112830407": response6,
-				"entry.1699263877": response7,
-				"entry.1101063328": response8,
-				"entry.914134514": response9,
-				"entry.2131069147": response10,
+				"entry.755042765": responseText1,
+				"entry.1471559929": responseText2,
+				"entry.1163044351": responseText3,
+				"entry.1384086246": responseText4,
+				"entry.879818529": responseText5,
+				"entry.2112830407": responseText6,
+				"entry.1699263877": responseText7,
+				"entry.1101063328": responseText8,
+				"entry.914134514": responseText9,
+				"entry.2131069147": responseText10,
 				"emailAddress": email
 			},
 			type: "POST",
