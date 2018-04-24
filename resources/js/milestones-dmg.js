53243e0bf435dc19fdd46b08fb8dc7d723b681d4
Jeff Chamblee <jeffchamblee@users.noreply.github.com>
2018-04-24 14:01:00 -0400

    Added Milestones DMG page

---
+++ resources/js/milestones-dmg.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var datasetTimeline = {
+    getData: function () {
+        var t = this;
+        $.getJSON("//jeffchamblee.github.io/resources/data/dmg-dataset-launch-dates.json", function (data) {
+            t.launchdates = data;
+            //console.log(t.launchdates.length);
+            datasetTimeline.timeline = milestones('#timeline')
+                .mapping({
+                    timestamp: 'date_time',
+                    text: 'title'
+                });
+          datasetTimeline.update();
+        });
+    },
+    update: function () {
+        datasetTimeline.timeline
+            .parseTime('%Y-%m-%dT%H:%M')
+            .aggregateBy(selectGroupBy.options[selectGroupBy.selectedIndex].value)
+            .optimize(true)
+            .render(datasetTimeline.launchdates);
+    }
+};
+datasetTimeline.getData();
+var selectGroupBy = document.getElementById('select-groupby');
+selectGroupBy.onchange = datasetTimeline.update;
